81c2fb86ab466ad025e4c001c35389009f7962b1
Kimmo Halonen <hemmoi@gmail.com>
2017-05-09 18:50:06 +0000

    set up public mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base   
+  default to: "info@example.com"      
+  
+  def contact_email(name, email, message)     
+    @name = name     
+    @email = email     
+    @message = message 
+ 
+  mail(from: email, subject: 'Example.com Contact Form Message')   
+  end 
+end
